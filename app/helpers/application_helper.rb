6fbc4f5e188fba661b3ac427b3e0d75da8395feb
loki148 <96773742+loki148@users.noreply.github.com>
2024-06-25 13:55:13 +0200

    Add transactions (#2)
    * Add transactions
    
    * Added styling
    
    * added validations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def active_For(path: nil, controller: nil)
+    if controller && controller_name == controller ||
+         path && request.path == path
+      'active'
+    end
+  end
+
+  def disable_links_if_not_signed()
+    'disabled' if !user_signed_in?
+  end
 end
