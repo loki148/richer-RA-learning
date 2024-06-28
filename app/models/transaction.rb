d8ce8c1d2d9ae3c0247aee651b92b8c11d00d7a1
loki148 <3luky3@gmail.com>
2024-06-28 09:42:16 +0200

    Add user id to transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,5 @@
 class Transaction < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true
   validates :amount, comparison: { greater_than: 0 }
   enum kind: { expense: 0, income: 1 }
